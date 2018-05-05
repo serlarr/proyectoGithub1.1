ae947e6d9d75d1e6e7428e20adbc650a9f9db991
serlarr <serlarr@conceptronic>
2018-05-05 09:55:40 +0200

    Primer commit de nuestro proyecto

---
+++ ProyectoGithub1.1/src/main/Clase1.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Clase1 {
+	private int atributo1;
+	private String atributo2;
+	private boolean atributo3;
+	
+	public Clase1(int atributo1, String atributo2, boolean atributo3) {
+		this.atributo1 = atributo1;
+		this.atributo2 = atributo2;
+		this.atributo3 = atributo3;
+	}
+	
+	public void saludo() {
+		System.out.println("Hola Ana");
+	}
+}
